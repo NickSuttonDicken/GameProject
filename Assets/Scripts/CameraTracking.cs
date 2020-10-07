ddcd933b274ea383c8783962ff1eac88b48614f3
Frozti <nicholassuttondicken@gmail.com>
2020-10-07 15:01:13 +1300

    Added Camera Tracking

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    public HeroScript hero;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(hero.transform.position.x, transform.position.y, hero.transform.position.z);
+    }
+
+}
