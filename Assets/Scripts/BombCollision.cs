abec76d0184deaa1a37ab27ff4616fa59eb1dc19
Frozti <nicholassuttondicken@gmail.com>
2020-10-07 16:29:10 +1300

    Added explosion stuff

---
+++ Assets/Scripts/BombCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombCollision : MonoBehaviour
+{
+    private int bombDamage = 20;
+    private GameObject explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosion = Resources.Load("Sphere") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Terrain")
+        {
+            Explosion();
+            DestroySelf();
+        }
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void Explosion()
+    {
+        GameObject obj = Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+}
