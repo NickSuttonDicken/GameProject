97283e3e43570ff2dd7da137324a1b1f9fe14b90
Frozti <nicholassuttondicken@gmail.com>
2020-10-11 23:52:32 +1300

    Added Player Attacking and Damage

---
+++ Assets/Scripts/HeroRadiusCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRadiusCollider : MonoBehaviour
+{
+    public HeroScript hero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            hero.AddEnemy(other.gameObject);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            hero.RemoveEnemy(other.gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.AI;
 
 public class HeroScript : MonoBehaviour
 {
+    private List<GameObject> enemyList = new List<GameObject>();
+
     public int maxHealth = 100;
     public PlayerHealthBar healthBar;
     public float originalSpeed;
@@ -12,7 +14,16 @@ public class HeroScript : MonoBehaviour
     private float modifiedSpeed;
     NavMeshAgent mesh;
     Animator anim;
-    
+
+    public int damage;
+    public float attackRate;
+    public float attackRange;
+    private float inRange;
+
+    public float timer;
+    Ray attackRay;
+    RaycastHit attackHit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,12 +48,57 @@ public class HeroScript : MonoBehaviour
         {
             HeroDeath();
         }
+
+        if (enemyList.Count != 0)
+        {
+            if (enemyList[0] == null)
+            {
+                Debug.Log("Null");
+                enemyList.RemoveAt(0);
+            }
+            else
+            {
+                Vector3 direction = enemyList[0].transform.position - transform.position;
+                Quaternion rotation = Quaternion.LookRotation(direction);
+                transform.rotation = rotation;
+
+                //mesh.destination = enemyList[0].transform.position;
+                timer += Time.deltaTime;
+                if (timer > attackRate)
+                {
+                    Debug.Log("Attacking");
+                    anim.Play("Male Attack 1");
+                    Attack();
+                    timer = 0;
+                }
+            }
+        }
     }
 
     public void WalkTo(Vector3 point)
     {
         mesh.destination = point;
         anim.SetInteger("HeroState", 2);
+    }
+
+    public void Attack()
+    {
+        attackRay.origin = transform.position;
+        attackRay.direction = transform.up;
+
+        if (enemyList.Count != 0)
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 toTarget = enemyList[0].transform.position - transform.position;
+            inRange = Vector3.Dot(forward, toTarget);
+
+            if (inRange > 0f && inRange < 1f)
+            {
+                attackRay.direction = enemyList[0].transform.position;
+                Physics.Raycast(attackRay, out attackHit, attackRange);
+                enemyList[0].GetComponent<EnemyScript>().healthBar.TakeDamage(damage);
+            }
+        }
     }
 
     public void PassHealth()
@@ -68,4 +124,15 @@ public class HeroScript : MonoBehaviour
         mesh.speed = currentSpeed;
     }
 
+    public void AddEnemy(GameObject other)
+    {
+        Debug.Log("Added to List");
+        enemyList.Add(other);
+    }
+
+    public void RemoveEnemy(GameObject other)
+    {
+        Debug.Log("Removed from List");
+        enemyList.Remove(other);
+    }
 }
