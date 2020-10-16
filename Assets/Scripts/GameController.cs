a8eb32ef0c419ea0776ac2957af5465942673f2d
Frozti <nicholassuttondicken@gmail.com>
2020-10-16 22:00:39 +1300

    Did stuff

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public HeroScript hero;
+    public GameObject spawn;
+    public List<GameObject> spawnSpots = new List<GameObject>();
+    public GameObject hero;
+    public HeroScript heroScript;
     public UIController UI;
     private string enemyCount;
     private string waveCount;
@@ -13,6 +16,11 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        spawnSpots[0] = spawn.transform.GetChild(0).gameObject;
+        spawnSpots[1] = spawn.transform.GetChild(1).gameObject;
+        spawnSpots[2] = spawn.transform.GetChild(2).gameObject;
+
+        hero.transform.position = spawnSpots[2].transform.position;
     }
 
     // Update is called once per frame
